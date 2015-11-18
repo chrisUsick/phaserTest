1abcda57b11175a2afc36b50f2a5156b89d59766
chrisUsick <christopher.usick@gmail.com>
2015-11-18 13:13:00 -0600

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,6 @@
+import msg from './msg';
+// import {Game} from './game';
+import {GameFactory} from './gameFactory';
+console.log(msg('Allo?'));
+const gameFac = new GameFactory();
+var game = gameFac.createGame();
