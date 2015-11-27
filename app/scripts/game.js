0e0975fe3ba2aa255ee7d3d2a3f891067a997e9f
chrisUsick <christopher.usick@gmail.com>
2015-11-27 14:11:49 -0600

    refactored code to use inheritence

---
+++ app/scripts/game.js
@@ -0,0 +1,44 @@
+/* initial state functions */
+
+class InitialState extends Phaser.State {
+  preload() {
+    this.game.load.image('mushroom', 'images/mushroom2.png');
+  }
+  create() {
+
+    this.game.stage.backgroundColor = '#2d2d2d';
+
+    //  Make our game world 2000x2000 pixels in size (the default is to match the game size)
+    this.game.world.setBounds(0, 0, 2000, 2000);
+
+    for (var i = 0; i < 150; i++) {
+      this.game.add.sprite(this.game.world.randomX, this.game.world.randomY, 'mushroom');
+    }
+  }
+  update() {
+    const cursors = this.game.input.keyboard.createCursorKeys();
+    if (cursors.up.isDown) {
+      this.game.camera.y -= 4;
+    }
+    else if (cursors.down.isDown) {
+      this.game.camera.y += 4;
+    }
+
+    if (cursors.left.isDown) {
+      this.game.camera.x -= 4;
+    }
+    else if (cursors.right.isDown) {
+      this.game.camera.x += 4;
+    }
+
+  }
+}
+
+export default class Game extends Phaser.Game {
+  constructor() {
+    super(800, 600, Phaser.CANVAS, 'phaser-example', new InitialState());
+  }
+  render() {
+    this.debug.cameraInfo(game.camera, 32, 32);
+  }
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 import msg from './msg';
+import Game from './game';
 // import {Game} from './game';
 import {GameFactory} from './gameFactory';
 console.log(msg('Allo?'));
-const gameFac = new GameFactory();
-var game = gameFac.createGame();
+// const gameFac = new GameFactory();
+// var game = gameFac.createGame();
+let game = new Game();
